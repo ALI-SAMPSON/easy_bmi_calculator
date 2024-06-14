6cd493e7a46a09227782efa2959c992d234839d5
Ali Sampson <alisampson2@gmail.com>
2024-06-14 16:35:32 +0000

    First Release

---
+++ lib/bmi_calculator.dart
@@ -0,0 +1,12 @@
+library easy_bmi_calculator;
+
+import 'package:intl/intl.dart';
+
+part 'easy_bmi_calculator.dart';
+part 'easy_bmi_formatter.dart';
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
---
+++ lib/easy_bmi_calculator.dart
@@ -0,0 +1,5 @@
+part of easy_bmi_calculator;
+
+double calculateBMI(double weight, double height) {
+  return weight / (height * height);
+}
